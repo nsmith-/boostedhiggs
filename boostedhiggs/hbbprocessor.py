7b2c00de321973cb43a5369fee948ae89f20e531
Nick Smith <nick.smith@cern.ch>
2020-02-19 23:03:46 -0600

    noweight

--- boostedhiggs/hbbprocessor.py
+++ boostedhiggs/hbbprocessor.py
@@ -88,6 +88,14 @@ class HbbProcessor(processor.ProcessorABC):
                 hist.Bin('msd', r'Jet $m_{sd}$', 23, 40, 201),
                 hist.Bin('ddb', r'Jet ddb score', [0, 0.89, 1]),
             ),
+            'genresponse_noweight': hist.Hist(
+                'Events',
+                hist.Cat('dataset', 'Dataset'),
+                hist.Cat('region', 'Region'),
+                hist.Cat('systematic', 'Systematic'),
+                hist.Bin('pt', r'Jet $p_{T}$ [GeV]', [450, 500, 550, 600, 675, 800, 1200]),
+                hist.Bin('genpt', r'Generated Higgs $p_{T}$ [GeV]', [200, 300, 450, 650, 7500]),
+            ),
             'genresponse': hist.Hist(
                 'Events',
                 hist.Cat('dataset', 'Dataset'),
@@ -251,6 +259,14 @@ class HbbProcessor(processor.ProcessorABC):
                 weight=weight,
             )
             if wmod is not None:
+                output['genresponse_noweight'].fill(
+                    dataset=dataset,
+                    region=region,
+                    systematic=sname,
+                    pt=normalize(candidatejet.pt),
+                    genpt=normalize(genBosonPt),
+                    weight=events.genWeight * wmod,
+                )
                 output['genresponse'].fill(
                     dataset=dataset,
                     region=region,
